f62faec37de798c806cab070a2413b57e67c23b7
Jair Zielinski <jairzielinski.med@gmail.com>
2025-08-16 00:05:06 -0300

    Subo archivo HTML

---
+++ HELLO.py
@@ -0,0 +1,12 @@
+print("Hello")
+nombre= "Jair"
+print("Hola_" + nombre )
+Nnumeor= 37
+print("Hola " + nombre + " tu edad es: " + str(Nnumeor))
+#hola aca es un comentario
+nombre= "Jair"
+
+
+# comentario 2
+ 
+
